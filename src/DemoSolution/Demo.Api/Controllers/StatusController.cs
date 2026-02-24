c72b32da7904416982c6ffafed8e313ac2bf793a
Kaung Lwin <kgmyatlwin89@gmail.com>
2026-02-24 14:41:08 +0000

    Add initial implementation of Demo.Api with status endpoint and configuration files

---
+++ src/DemoSolution/Demo.Api/Controllers/StatusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Api.Controllers;
+
+public class StatusController : ControllerBase
+{
+    [HttpGet("/status")]
+    public async Task<ActionResult> GetTheStatusAsync()
+    {
+        return Ok("Look Good");
+    }
+}
